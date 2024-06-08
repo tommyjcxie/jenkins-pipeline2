8747b9484c6cc4944b57f8d8c9b0aef15b7861d3
Tommy Xie <45479889+tommyjcxie@users.noreply.github.com>
2024-06-07 23:09:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "us-east-2"  
 }
 
-resource "aws_instance" "instance" {
+resource "aws_instance" "wordpress_instance" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
