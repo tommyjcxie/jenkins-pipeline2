abd09ee18065d4a7606f2af668b8792fcb6b35f1
Tommy Xie <45479889+tommyjcxie@users.noreply.github.com>
2024-06-07 23:17:01 -0400

    deploy to us-east-2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"  
 }
 
-resource "aws_instance" "wordpress_instance" {
+resource "aws_instance" "wordpress_instances" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
