c0f61d0267b1a4860371ba8cde27749a71ea4a4b
Tommy Xie <45479889+tommyjcxie@users.noreply.github.com>
2024-06-09 14:43:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,52 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# Data source to fetch the existing Elastic IP
-data "aws_eip" "existing_eip" {
-  public_ip = "13.58.73.5" # Use elastic IP from aws
+# Data source to fetch the default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_security_group" "allow_web_traffic" {
-  name        = "allow-web-traffic"
-  description = "Allow HTTP/HTTPS inbound traffic"
+# Data source to fetch the default subnets in the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "allow-alb-traffic"
+  description = "Allow HTTP/HTTPS inbound traffic for ALB"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access from any IP address
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS access from any IP address
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for EC2 instances
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow-ec2-traffic"
+  description = "Allow HTTP/HTTPS inbound traffic for EC2 instances"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -40,20 +78,81 @@ resource "aws_security_group" "allow_web_traffic" {
   }
 }
 
-# Create a new EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0ca2e925753ca2fb4" # Amazon Linux AMI in us-east-2
-  instance_type = "t2.micro"
-  key_name      = "ansible-kp"
-  vpc_security_group_ids = [aws_security_group.allow_web_traffic.id] # Associate the security group
+# Create a Load Balancer
+resource "aws_lb" "wordpress_lb" {
+  name               = "wordpress-tf-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids
+}
 
-  tags = {
-    Name = "Tommy-Jenkins"
+# Create a Target Group
+resource "aws_lb_target_group" "wordpress_tg" {
+  name     = "wordpress-tf-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
   }
 }
 
-# Associate the existing Elastic IP with the EC2 instance
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.example.id
-  allocation_id = data.aws_eip.existing_eip.id
+# Create a Listener for the Load Balancer
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.wordpress_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress_tg.arn
+  }
 }
+
+# Create a Launch Template
+resource "aws_launch_template" "wordpress_lt" {
+  name_prefix   = "wordpress-"
+  image_id      = "ami-05901cf722e806632" # Custom Wordpress AMI
+  instance_type = "t2.micro"
+  key_name      = "ansible-kp"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.ec2_sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "Tommy-Jenkins"
+    }
+  }
+}
+
+# Create an Auto Scaling Group
+resource "aws_autoscaling_group" "wordpress_asg" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  launch_template {
+    id      = aws_launch_template.wordpress_lt.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.wordpress_tg.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-wp"
+    propagate_at_launch = true
+  }
+}
+
