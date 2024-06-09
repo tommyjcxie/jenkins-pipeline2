d0ddc62a2f12fa6088753173b2ade85e8033258c
Tommy Xie <45479889+tommyjcxie@users.noreply.github.com>
2024-06-09 12:10:57 -0400

    associate IP

--- main.tf
+++ main.tf
@@ -2,9 +2,28 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow-ssh"
-  description = "Allow SSH inbound traffic"
+# Data source to fetch the existing Elastic IP
+data "aws_eip" "existing_eip" {
+  public_ip = "13.58.73.5" # Replace with your existing Elastic IP
+}
+
+resource "aws_security_group" "allow_web_traffic" {
+  name        = "allow-web-traffic"
+  description = "Allow HTTP/HTTPS inbound traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP access from any IP address
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS access from any IP address
+  }
 
   ingress {
     from_port   = 22
@@ -14,21 +33,27 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # Create a new EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0c9921088121ad00b" # Amazon Linux 2 AMI in us-east-2
-  instance_type = "t2.micro" 
-  key_name      = "ansible-kp" 
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id] # Associate the security group
+  instance_type = "t2.micro"
+  key_name      = "ansible-kp"
+  vpc_security_group_ids = [aws_security_group.allow_web_traffic.id] # Associate the security group
 
   tags = {
     Name = "Tommy-Jenkins"
   }
+}
+
+# Associate the existing Elastic IP with the EC2 instance
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.example.id
+  allocation_id = data.aws_eip.existing_eip.id
 }
